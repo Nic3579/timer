c0bb6526bde6754a6bbcb6f11513e6d8384d1f10
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-10-28 14:16:55 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,20 @@ let min = 0
 let sec = 0
 let alarm = 0
 input.onButtonPressed(Button.A, function () {
-    min = 10
+    // amt. of time the timer goes for in min.
+    min = 15
     sec = 0
-    while (min + 0 > 0) {
+    // timer still has time left? if so, return true
+    while (min + sec > 0) {
+        // every 60 sec it removes a min
+        // and resets seconds
+        if (sec == 0) {
+            min += -1
+            sec = 0
+        }
         basic.pause(1000)
         sec += -1
+        // show amt. of time left
         basic.showString("" + min + ":" + sec)
         basic.clearScreen()
     }
@@ -16,11 +25,7 @@ input.onButtonPressed(Button.B, function () {
     alarm = 0
 })
 basic.forever(function () {
-    // every 60 sec
-    if (sec == 0) {
-        min += -1
-        sec = 0
-    }
+    // sets off a alarm when time runs out
     if (alarm == 1) {
         music.playMelody("F E F E - G - G ", 120)
     }
